47d242ccea0b67c1802f02be1efbe69046e17fc8
Q Hardy <qhardy1@gmail.com>
2018-10-02 14:16:43 -0400

    routing

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require("express");
 const app = express();
 const hbs = require("hbs");
 const bodyParser = require("body-parser");
-const router = require('./routes/index.js')
 
 app.set('view engine', 'hbs')
-app.use('router')
-app.use(require("./routes/index.js"));
+//app.use('router')
 
 
 
--- routes/application.js
+++ routes/application.js
@@ -3,9 +3,12 @@ const router = express.Router()
 
 
 
+app.get('/', (req, res) => {
+    res.render('/layout.hbs')
+})
 
 
-// router.use('/', require('./application.js'));
+router.use('/', require('./application.js'));
 router.use('/userTrack', require('./userTrack'));
 router.use('/venue', require('./venue'));
 
